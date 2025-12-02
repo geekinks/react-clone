dcb4a95c0bda48a2df68262c3a8cea9179f34006
ADAMU MUHAMMAD MUHAMMAD <muhdadamu0008@gmail.com>
2025-12-02 12:21:04 +0100

    Add routing and shared components for navigation
    Introduced react-router for client-side routing and added new pages (Home, Teams, Courses, Internships, Blogs). Created shared Navbar and Footer components, added navbar styles, and updated App and main entry to support navigation. Also refactored Hero component location and import paths.

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Blogs from "./pages/Blogs";
+import Courses from "./pages/Courses";
+import Home from "./pages/Home";
+import Internships from "./pages/Internships";
+import Teams from "./pages/Teams";
+
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App/>
+    <BrowserRouter>
+        <App/>
+       <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/teams" element={<Teams />} />
+          <Route path="/courses" element={<Courses />} />
+          <Route path="/internships" element={<Internships />} />
+          <Route path="/blogs" element={<Blogs />} />
+       </Routes>
+    </BrowserRouter>  
   </React.StrictMode>
 );
